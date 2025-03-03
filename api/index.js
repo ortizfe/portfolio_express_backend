067bc30fc7c53fd19317e386faf6479d709c55c4
ortizfe <79375454+ortizfe@users.noreply.github.com>
2025-03-03 18:55:07 -0500

    added spotify routes

--- api/index.js
+++ api/index.js
@@ -1,56 +1,18 @@
 const express = require("express");
+const spotify_router = require("./spotify");
+
 const app = express();
-const axios = require("axios");
-const dotenv = require("dotenv");
-dotenv.configDotenv();
+const PORT = 3000;
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 // /* Spotify API Calls */
-// const spotify_client_id = import.meta.env.SPOTIFY_CLIENT_ID;
-// const spotify_client_secret = import.meta.env.SPOTIFY_CLIENT_SECRET;
-
-// const access_token_header = {
-//   headers: {
-//     "Content-Type": "application/x-www-form-urlencoded",
-//   },
-// };
-
-// const access_token = await axios.post(
-//   "https://accounts.spotify.com/api/token",
-//   `grant_type=client_credentials&client_id=${spotify_client_id}&client_secret=${spotify_client_secret}`,
-//   access_token_header
-// );
-
-// const auth_header = {
-//   headers: {
-//     Authorization: `Bearer ${access_token.data.access_token}`,
-//   },
-// };
-
-// // getting artist by artist ID
-// app.get("/artist", async (req, res) => {
-//   const artist = await axios.get(
-//     `https://api.spotify.com/v1/artists/1btWGBz4Uu1HozTwb2Lm8A`,
-//     auth_header
-//   );
-//   res.json(artist.data);
-// });
-
-// // get artists albums
-// app.get("/artist/albums", async (req, res) => {
-//   const albums = await axios.get(
-//     `https://api.spotify.com/v1/artists/1btWGBz4Uu1HozTwb2Lm8A/albums?include_groups=album`,
-//     auth_header
-//   );
-
-//   res.json(albums.data.items);
-// });
+app.use("/spotify", spotify_router);
 
 /* The Movie Database API Calls */
 
 /* Market Stack API Calls */
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
 
 module.exports = app;
--- api/spotify.js
+++ api/spotify.js
@@ -1,44 +1,49 @@
 /* Spotify API Calls */
-// import axios from "axios";
-// import dotenv from "dotenv";
-// dotenv.configDotenv();
+// const axios = require("axios");
+const express = require("express");
+const spotify_router = express.Router();
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.configDotenv();
 
-// const spotify_client_id = import.meta.env.SPOTIFY_CLIENT_ID;
-// const spotify_client_secret = import.meta.env.SPOTIFY_CLIENT_SECRET;
+const spotify_client_id = import.meta.env.SPOTIFY_CLIENT_ID;
+const spotify_client_secret = import.meta.env.SPOTIFY_CLIENT_SECRET;
 
-// const access_token_header = {
-//   headers: {
-//     "Content-Type": "application/x-www-form-urlencoded",
-//   },
-// };
+const access_token_header = {
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+};
 
-// const access_token = await axios.post(
-//   "https://accounts.spotify.com/api/token",
-//   `grant_type=client_credentials&client_id=${spotify_client_id}&client_secret=${spotify_client_secret}`,
-//   access_token_header
-// );
+const access_token = await axios.post(
+  "https://accounts.spotify.com/api/token",
+  `grant_type=client_credentials&client_id=${spotify_client_id}&client_secret=${spotify_client_secret}`,
+  access_token_header
+);
 
-// const auth_header = {
-//   headers: {
-//     Authorization: `Bearer ${access_token.data.access_token}`,
-//   },
-// };
+const auth_header = {
+  headers: {
+    Authorization: `Bearer ${access_token.data.access_token}`,
+  },
+};
 
-// // getting artist by artist ID
-// app.get("/artist", async (req, res) => {
-//   const artist = await axios.get(
-//     `https://api.spotify.com/v1/artists/1btWGBz4Uu1HozTwb2Lm8A`,
-//     auth_header
-//   );
-//   res.json(artist.data);
-// });
+// getting artist by artist ID
+spotify_router.get("/artist", async (req, res) => {
+  const artist = await axios.get(
+    `https://api.spotify.com/v1/artists/1btWGBz4Uu1HozTwb2Lm8A`,
+    auth_header
+  );
+  res.json(artist.data);
+});
 
-// // get artists albums
-// app.get("/artist/albums", async (req, res) => {
-//   const albums = await axios.get(
-//     `https://api.spotify.com/v1/artists/1btWGBz4Uu1HozTwb2Lm8A/albums?include_groups=album`,
-//     auth_header
-//   );
+// get artists albums
+spotify_router.get("/albums", async (req, res) => {
+  const albums = await axios.get(
+    `https://api.spotify.com/v1/artists/1btWGBz4Uu1HozTwb2Lm8A/albums?include_groups=album`,
+    auth_header
+  );
 
-//   res.json(albums.data.items);
-// });
+  res.json(albums.data.items);
+});
+
+module.exports = spotify_router;
