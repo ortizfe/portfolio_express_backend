050dfb0ade94f709ba0ac1b5f862650668c87bb6
ortizfe <79375454+ortizfe@users.noreply.github.com>
2025-03-03 19:59:43 -0500

    updating code to work for spotify api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
 const spotify_router = require("./spotify");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
+
+// Rate limiter: 20 requests per minute per IP
+const spotifyRateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  limit: 20, // Max 20 requests per IP
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  message: "Too many requests, please try again later.",
+});
+app.use(spotifyRateLimiter);
+app.set("trust proxy", 1);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
--- api/spotify.js
+++ api/spotify.js
@@ -14,26 +14,43 @@ const access_token_header = {
   },
 };
 
-// const access_token = await axios.post(
-//   "https://accounts.spotify.com/api/token",
-//   `grant_type=client_credentials&client_id=${spotify_client_id}&client_secret=${spotify_client_secret}`,
-//   access_token_header
-// );
+async function getAccessToken() {
+  try {
+    const response = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      `grant_type=client_credentials&client_id=${spotify_client_id}&client_secret=${spotify_client_secret}`,
+      access_token_header
+    );
 
-// const auth_header = {
-//   headers: {
-//     Authorization: `Bearer ${access_token.data.access_token}`,
-//   },
-// };
+    return response.data.access_token;
+  } catch (error) {
+    console.error(
+      "Error getting access token: ",
+      error.response?.data || error.message
+    );
+  }
+}
 
-// // getting artist by artist ID
-// spotify_router.get("/artist", async (req, res) => {
-//   const artist = await axios.get(
-//     `https://api.spotify.com/v1/artists/1btWGBz4Uu1HozTwb2Lm8A`,
-//     auth_header
-//   );
-//   res.json(artist.data);
-// });
+// getting artist by artist ID
+spotify_router.get("/artist", async (req, res) => {
+  try {
+    const access_token = await getAccessToken();
+    const auth_header = {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+      timeout: 10000,
+    };
+
+    const artist = await axios.get(
+      `https://api.spotify.com/v1/artists/1btWGBz4Uu1HozTwb2Lm8A`,
+      auth_header
+    );
+    res.json(artist.data);
+  } catch (error) {
+    console.error("It didn't work");
+  }
+});
 
 // // get artists albums
 // spotify_router.get("/albums", async (req, res) => {
